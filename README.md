390822781827b57d12d94543521dc28971928252
Luiz Falcao <luizotavio-falcao@hotmail.com>
2021-12-12 16:37:50 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Clubhouse
+
+## About
+
+Web app where users can write anonymous posts. Once logged in, depending on their membership status, they're able to post and also view the author of any message.
+The project uses Node, Express, MongoDB, and EJS.
---
+++ app.js
@@ -0,0 +1,31 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+
+const indexRouter = require('./routes/index');
+
+const app = express();
+
+// View engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Middlewares
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.use('/', indexRouter);
+
+// 404 Handler
+app.use((req, res, next) => next(createError(404)));
+
+// Error handler
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(3000, () => console.log('App running on port 3000!'));

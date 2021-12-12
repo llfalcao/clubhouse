1a39cf82064697e0f19605de29c1252de1b42bd4
Luiz Falcao <luizotavio-falcao@hotmail.com>
2021-12-12 18:40:19 -0300

    Create User model

---
+++ models/User.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  first_name: { type: String, required: true },
+  last_name: { type: String, required: true },
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  membership_status: { type: String, required: true },
+});
+
+module.exports = mongoose.model('User', UserSchema);

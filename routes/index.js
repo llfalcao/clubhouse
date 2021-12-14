83a59ee4014ecf5b6e5e646d2793202e9f5665cf
Luiz Falcao <luizotavio-falcao@hotmail.com>
2021-12-14 20:54:49 -0300

    Display new message form

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
+// GET - Homepage
 router.get('/', (req, res) => {
   res.render('index', { title: 'Clubhouse', user: res.locals.currentUser });
 });
 
+// GET - Logout
 router.get('/logout', (req, res) => {
   req.logout();
   res.redirect('/');

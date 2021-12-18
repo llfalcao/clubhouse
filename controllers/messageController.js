c623b783a8462c86eef5a3b981d4f90a21ec7c26
Luiz Falcao <luizotavio-falcao@hotmail.com>
2021-12-18 17:07:10 -0300

    Refactoring

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -2,57 +2,55 @@ const { body, validationResult } = require('express-validator');
 
 const Message = require('../models/Message');
 
+async function getMessages() {
+  return await Message.find({})
+    .sort({ timestamp: -1 })
+    .populate({
+      path: 'author',
+      select: ['username', 'membership_status'],
+    })
+    .exec();
+}
+
 // Display messages on GET
 exports.messageListGET = async (req, res, next) => {
-  try {
-    const messages = await Message.find({})
-      .populate({ path: 'author', select: ['username', 'membership_status'] })
-      .sort({ timestamp: -1 })
-      .exec();
-    res.render('index', {
-      title: 'Clubhouse',
-      user: res.locals.currentUser,
-      messages,
-    });
-  } catch (err) {
-    return next(err);
-  }
+  const messages = await getMessages();
+  res.render('index', {
+    title: 'Clubhouse',
+    user: res.locals.currentUser,
+    messages,
+  });
 };
 
 // Handle new message on POST
 exports.messageCreatePOST = [
   body('title', 'Title required').trim().isLength({ min: 1, max: 100 }),
   body('text', 'Message required').trim().isLength({ min: 1, max: 280 }),
-  (req, res, next) => {
-    const user = JSON.parse(req.body.user);
+  async (req, res, next) => {
     const errors = validationResult(req);
     const message = new Message({
       title: req.body.title,
       text: req.body.text,
       timestamp: new Date(),
-      author: user._id,
+      author: res.locals.currentUser._id,
     });
 
     if (!errors.isEmpty()) {
+      const messages = await getMessages();
       res.render('index', {
         title: 'Clubhouse',
-        user: req.body.user,
+        user: res.locals.currentUser,
         message,
+        messages,
         errors: errors.array(),
       });
     } else {
       message.save(async (err) => {
         if (err) return next(err);
-        const messages = await Message.find({})
-          .sort({ timestamp: -1 })
-          .populate({
-            path: 'author',
-            select: ['username', 'membership_status'],
-          })
-          .exec();
+        const messages = await getMessages();
         res.render('index', {
           title: 'Clubhouse',
-          user: req.body.user,
+          user: res.locals.currentUser,
           messages,
         });
       });

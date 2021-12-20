6bf48f836eae9f33a184efdf06c7e09f0e893f26
Luiz Falcao <luizotavio-falcao@hotmail.com>
2021-12-19 22:05:46 -0300

    Set alternative port

--- app.js
+++ app.js
@@ -108,4 +108,6 @@ app.use((err, req, res, next) => {
   res.render('error');
 });
 
-app.listen(3000, () => console.log('App running on port 3000!'));
+app.listen(process.env.PORT || 3000, () =>
+  console.log('App running on port 3000!'),
+);
